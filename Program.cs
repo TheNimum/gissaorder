35ffa3c24048008d9d843bf34c41ab5b6a5a05ca
Mikael Pajari <mikael.pajari@live.se>
2020-09-15 20:30:40 +0200

    färdig

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace gissaorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretword = "hello";
+            string guesseword = "";
+            int guess = 0;
+            int guessmax = 3;
+            bool outofguess = false;
+                while (guesseword !=secretword && !outofguess)
+                {
+                    if ( guess < guessmax)
+                    {
+                        Console.Write("skriv det hemliga ordet.: ");
+                        guesseword = Console.ReadLine();
+                        guess++;
+                    }else 
+                    {
+                        outofguess = true;
+                    }
+                  }  
+                    if(outofguess)
+                    {
+                       Console.WriteLine("du förlorade :("); 
+                    }
+                    else
+                    {
+                        Console.WriteLine("korrekt ! :)");
+                    }
+                
+        }
+    }
+}
